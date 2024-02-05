88783816b9a7e4298dbca050d3fe294f65af59b3
Steve de p <steve.depeijper@gmail.com>
2024-02-05 10:38:07 +0100

    eerste

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-import React from 'react';
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+import { useState, useEffect } from "react";
+import axios from 'axios';
+
+export default function App() {
+  const [cijfers, setCijfers] = useState([]);
+
+  useEffect(() => {
+    console.log('useEffect');
+    axios.get('https://testcontainerbi2a2.calmmushroom-0deeca29.northeurope.azurecontainerapps.io/api/http_trigger')
+      .then(response => {    
+        setCijfers(response.data);
+        console.log(response.data);
+      });
+  }, []);
+
+  return (
+    <table>
+      <tbody>
+      {cijfers.map((cijfer) => (
+        <tr>
+          <td>{cijfer.vak_omschrijving}</td>
+          <td>{cijfer.cijfer}</td>
+        </tr>
+      ))}
+      </tbody>
+    </table>
+  );
 }
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { StrictMode } from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+  rootElement
+);
